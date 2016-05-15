654bbd338a8309c2782e0a335dbfa726ad5db03a
solneiiiko <solneiiiko@gmail.com>
2016-05-15 15:29:47 +0500

    hello app

--- init.sh
+++ init.sh
@@ -3,3 +3,4 @@ sudo /etc/init.d/nginx restart
 sudo cp etc/hello.py /etc/gunicorn.d/hello.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 # sudo gunicorn -b 0.0.0.0:8080 hello:app
+sudo /etc/init.d/gunicorn restart
