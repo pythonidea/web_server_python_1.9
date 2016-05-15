3a37db4a2e89ff0938aab151d72f176b3caab86a
solneiiiko <solneiiiko@gmail.com>
2016-05-15 16:28:06 +0500

    1.9

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
 def app(environ, start_response):
-    status = '200 OK'
-    headers = [
-        ('Content-Type', 'text/plain')
-    ]
-    body = '\n'.join(environ['QUERY_STRING'].split('&'))
-    start_response(status, headers )
-    return [body]
+	status = '200 OK'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	body = '\n'.join(environ['QUERY_STRING'].split('&'))
+	start_response(status, headers )
+	return [ body ]
