caaf8f980a99963a3dd62e9a23b842abc3907b1f
solneiiiko <solneiiiko@gmail.com>
2016-05-15 16:39:56 +0500

    server

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
-sudo ﻿ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo cp etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ﻿ln -s etc/hello.py /etc/gunicorn.d/hello.py
+sudo cp etc/hello.py /etc/gunicorn.d/hello.py
 # sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 sudo gunicorn -b 0.0.0.0:8080 hello:app
 sudo /etc/init.d/gunicorn restart
