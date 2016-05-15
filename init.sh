5a9c40f87f7f0d36d746f57360e126e4e1016013
solneiiiko <solneiiiko@gmail.com>
2016-05-15 15:11:22 +0500

    настройка сервера

---
+++ init.sh
@@ -0,0 +1,5 @@
+sudo cp etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo cp etc/hello.py /etc/gunicorn.d/hello.py
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+# sudo gunicorn -b 0.0.0.0:8080 hello:app
